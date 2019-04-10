cee6a800e120b6b087e3322ae5c21c9ca3d7dd35
cocobrune21 <corinne.gonneau@gmail.com>
2019-04-11 01:19:13 +0200

    Pagination  first-part

--- view/index.php
+++ view/index.php
@@ -4,23 +4,98 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    
+    <!-- Bootstrap -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-    <!-- Title -->
+    <!--Fontawesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
     <title>Billet simple pour l'Alaska</title>
-    <link href="style.css" rel="stylesheet" />
+    <link href="../public/css/style.css" rel="stylesheet" />
+
 </head>
 
 <body>
-
-    <?php
+    <!--  ?php
     require 'vendor/autoload.php';
-    ?>
+?  -->
 
-    <h1>Billet simple pour l'Alaska</h1>
+    <header>
+        <h1>Billet simple pour l'Alaska</h1>
 
+    </header>
+
+
+
+    <div class="row container_chapter">
+       
+
+        <div class="col-lg-8 chapter">
+            <h2 class="display-4">Ici le titre du chapitre</h2>
+            <hr class="my-4">
+            <p class="lead">Ici le texte </p>
+        </div>
+    
+        
+
+        <div class="card col-lg-3 login">
+            <div class="card-header">
+                <h3>Se connecter</h3>
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="pseudo">
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password" class="form-control" placeholder="mot de passe">
+                    </div>
+                    <div class="row align-items-center remember">
+                        <input type="checkbox">Se souvenir de moi
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Login" class="btn float-right login_btn">
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center links">
+                    Vous n'avez pas de compte ?<a href="#">S'enregistrer</a>
+                </div>
+            </div>
+        </div>
+        
+        
+    </div>
+
+
+    <footer class="row">
+        <div class="col-lg-12">
+            Pied de page
+        </div>
+    </footer>
+
+
+
+    <!-- Bootstrap -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
 
 </html>
