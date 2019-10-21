13325905251f74f70fdf909812e6c8aa92626b01
cocobrune21 <corinne.gonneau@gmail.com>
2019-10-21 22:02:24 +0200

    add config.php for Manage.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$hostname = getenv('SERVER_NAME');
+
+define('dbHost', $hostname);
+define('dbBase', 'projet_4');
+define('dbUser', 'root');
+define('dbPassword', '');
--- model/Manage.php
+++ model/Manage.php
@@ -1,11 +1,14 @@
 <?php
 
+require_once 'config.php';
+
 class Manage
 {
     protected function dbConnect()
     {
+        $dsn = 'mysql:host='.dbHost.';dbname='.dbBase;
         try {
-            $db = new PDO('mysql:host=localhost;dbname=projet_4;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $db = new PDO($dsn, dbUser, dbPassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
             return $db;
         } catch (Exception $error) {
